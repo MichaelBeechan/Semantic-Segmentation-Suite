f1b8fd36cfb31ea1c96bc4a2aeb9d3f57bbfc1a4
Roy Shilkrot <roys@cs.stonybrook.edu>
2018-11-09 22:44:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -31,12 +31,17 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess=tf.Session(config=config)
 
-net_input = tf.placeholder(tf.float32,shape=[None,None,None,3])
+net_input = tf.placeholder(tf.float32,shape=[None,None,None,4])
 net_output = tf.placeholder(tf.float32,shape=[None,None,None,num_classes]) 
 
 network, _ = model_builder.build_model(args.model, net_input=net_input, num_classes=num_classes, crop_width=args.crop_width, crop_height=args.crop_height, is_training=False)
 
+test_miou, test_miou_op = tf.metrics.mean_iou(labels=tf.argmax(net_output, -1),   
+                                              predictions=tf.argmax(network, -1),
+                                              num_classes=num_classes)
+
 sess.run(tf.global_variables_initializer())
+sess.run(tf.local_variables_initializer())
 
 print('Loading model checkpoint weights ...')
 saver=tf.train.Saver(max_to_keep=1000)
@@ -44,33 +49,50 @@ saver.restore(sess, args.checkpoint_path)
 
 # Load the data
 print("Loading the data ...")
-train_input_names,train_output_names, val_input_names, val_output_names, test_input_names, test_output_names = utils.prepare_data(dataset_dir=args.dataset)
+train_input_names, train_input_depth_names, train_output_names, val_input_names, val_input_depth_names, val_output_names, test_input_names, test_input_depth_names, test_output_names = utils.prepare_data_with_depth(dataset_dir=args.dataset)
 
 # Create directories if needed
 if not os.path.isdir("%s"%("Test")):
         os.makedirs("%s"%("Test"))
+        
+num_test_images = len(test_input_names) 
 
-target=open("%s/test_scores.csv"%("Test"),'w')
-target.write("test_name, test_accuracy, precision, recall, f1 score, mean iou, %s\n" % (class_names_string))
-scores_list = []
-class_scores_list = []
-precision_list = []
-recall_list = []
-f1_list = []
-iou_list = []
+print(test_input_names[0])
+
+# calculate the start crop coords for the exact middle of the image
+in_h,in_w = cv2.imread(test_input_names[0]).shape[:2]
+crp_xs = int(in_w/2)-int(args.crop_width/2)
+crp_ys = int(in_h/2)-int(args.crop_height/2)
+crp_xe = int(crp_xs + args.crop_width)
+crp_ye = int(crp_ys + args.crop_height)
+
 run_times_list = []
 
 # Run testing on ALL test images
-for ind in range(len(test_input_names)):
-    sys.stdout.write("\rRunning test image %d / %d"%(ind+1, len(test_input_names)))
+for ind in range(num_test_images):
+    sys.stdout.write("\rRunning test image %d / %d: "%(ind+1, num_test_images))
     sys.stdout.flush()
 
-    input_image = np.expand_dims(np.float32(utils.load_image(test_input_names[ind])[:args.crop_height, :args.crop_width]),axis=0)/255.0
-    gt = utils.load_image(test_output_names[ind])[:args.crop_height, :args.crop_width]
-    gt = helpers.reverse_one_hot(helpers.one_hot_it(gt, label_values))
+    input_image = utils.load_image(test_input_names[ind])
+
+    # add the depth as the last channel to input
+    input_depth_image = utils.load_image(test_input_depth_names[ind])
+    if len(input_depth_image.shape) == 3 and input_depth_image.shape[2] == 3:
+        input_depth_image = cv2.cvtColor(input_depth_image, cv2.COLOR_BGR2GRAY)
+    input_image = np.dstack((input_image, input_depth_image))
+    
+    input_image = input_image[crp_ys:crp_ye,crp_xs:crp_xe]
+    input_image = np.expand_dims(np.float32(input_image),axis=0)/255.0
+
+    gt = utils.load_image(test_output_names[ind])[crp_ys:crp_ye,crp_xs:crp_xe]
+    gt = np.expand_dims(helpers.one_hot_it(gt, label_values, tolerance=50), axis=0)
 
     st = time.time()
-    output_image = sess.run(network,feed_dict={net_input:input_image})
+    
+    output_image,curr_test_miou_op = sess.run([network,test_miou_op],feed_dict={net_input:input_image,net_output:gt})
+    curr_test_miou = sess.run(test_miou,feed_dict={net_input:input_image,net_output:gt})
+    
+    print("Current mIOU %.3f"%(curr_test_miou))
 
     run_times_list.append(time.time()-st)
 
@@ -78,42 +100,13 @@ for ind in range(len(test_input_names)):
     output_image = helpers.reverse_one_hot(output_image)
     out_vis_image = helpers.colour_code_segmentation(output_image, label_values)
 
-    accuracy, class_accuracies, prec, rec, f1, iou = utils.evaluate_segmentation(pred=output_image, label=gt, num_classes=num_classes)
+    gt = helpers.colour_code_segmentation(helpers.reverse_one_hot(gt[0,:,:,:]), label_values)
 
-    file_name = utils.filepath_to_name(test_input_names[ind])
-    target.write("%s, %f, %f, %f, %f, %f"%(file_name, accuracy, prec, rec, f1, iou))
-    for item in class_accuracies:
-        target.write(", %f"%(item))
-    target.write("\n")
+    file_name = os.path.splitext(os.path.basename(test_input_names[ind]))[0]
+    file_dir = "%s/%s_%s"%("Test", args.model, os.path.basename(args.dataset))
+    cv2.imwrite("%s/%s_pred.png"%(file_dir, file_name),cv2.cvtColor(np.uint8(out_vis_image), cv2.COLOR_RGB2BGR))
+    cv2.imwrite("%s/%s_gt.png"%(file_dir, file_name),cv2.cvtColor(np.uint8(gt), cv2.COLOR_RGB2BGR))
 
-    scores_list.append(accuracy)
-    class_scores_list.append(class_accuracies)
-    precision_list.append(prec)
-    recall_list.append(rec)
-    f1_list.append(f1)
-    iou_list.append(iou)
-    
-    gt = helpers.colour_code_segmentation(gt, label_values)
-
-    cv2.imwrite("%s/%s_pred.png"%("Test", file_name),cv2.cvtColor(np.uint8(out_vis_image), cv2.COLOR_RGB2BGR))
-    cv2.imwrite("%s/%s_gt.png"%("Test", file_name),cv2.cvtColor(np.uint8(gt), cv2.COLOR_RGB2BGR))
-
-
-target.close()
-
-avg_score = np.mean(scores_list)
-class_avg_scores = np.mean(class_scores_list, axis=0)
-avg_precision = np.mean(precision_list)
-avg_recall = np.mean(recall_list)
-avg_f1 = np.mean(f1_list)
-avg_iou = np.mean(iou_list)
-avg_time = np.mean(run_times_list)
-print("Average test accuracy = ", avg_score)
-print("Average per class test accuracies = \n")
-for index, item in enumerate(class_avg_scores):
-    print("%s = %f" % (class_names_list[index], item))
-print("Average precision = ", avg_precision)
-print("Average recall = ", avg_recall)
-print("Average F1 score = ", avg_f1)
-print("Average mean IoU score = ", avg_iou)
-print("Average run time = ", avg_time)
+print("Overall mIOU %.3f"%(curr_test_miou))
+print(curr_test_miou_op)
+print("Average inference time %.3f"%(np.mean(run_times_list)))
