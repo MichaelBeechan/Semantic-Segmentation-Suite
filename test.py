0ed9ee5cefa93efc8421dcdea41334fb8d9f801a
Roy Shilkrot <roys@cs.stonybrook.edu>
2018-11-10 01:10:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--checkpoint_path', type=str, default=None, required=True, help='The path to the latest checkpoint weights for your model.')
 parser.add_argument('--crop_height', type=int, default=512, help='Height of cropped input image to network')
 parser.add_argument('--crop_width', type=int, default=512, help='Width of cropped input image to network')
+parser.add_argument('--num_images', type=int, default=-1, help='Number of images to test. -1 for all images')
 parser.add_argument('--model', type=str, default=None, required=True, help='The model you are using')
 parser.add_argument('--dataset', type=str, default="CamVid", required=False, help='The dataset you are using')
+parser.add_argument('--outdir_prefix', type=str, default="", required=False, help='Prefix to add to output directory')
 args = parser.parse_args()
 
 # Get the names of the classes so we can record the evaluation results
@@ -55,9 +57,7 @@ train_input_names, train_input_depth_names, train_output_names, val_input_names,
 if not os.path.isdir("%s"%("Test")):
         os.makedirs("%s"%("Test"))
         
-num_test_images = len(test_input_names) 
-
-print(test_input_names[0])
+num_test_images = len(test_input_names) if args.num_images == -1 else int(args.num_images)
 
 # calculate the start crop coords for the exact middle of the image
 in_h,in_w = cv2.imread(test_input_names[0]).shape[:2]
@@ -68,15 +68,27 @@ crp_ye = int(crp_ys + args.crop_height)
 
 run_times_list = []
 
+model_name = args.model
+if len(args.outdir_prefix) > 0:
+    model_name = args.outdir_prefix + "_" + model_name
+    
+dataset_dir = args.dataset if args.dataset[-1] != "/" else args.dataset[:-1]
+
+file_dir = "%s/%s_%s"%("Test", model_name, os.path.basename(dataset_dir))
+if not os.path.isdir(file_dir):
+    os.makedirs(file_dir)
+
+img_ind_shuffle = np.random.permutation(len(test_input_names))
+    
 # Run testing on ALL test images
-for ind in range(num_test_images):
+for ind,img_ind in enumerate(img_ind_shuffle[:num_test_images]):
     sys.stdout.write("\rRunning test image %d / %d: "%(ind+1, num_test_images))
     sys.stdout.flush()
 
-    input_image = utils.load_image(test_input_names[ind])
+    input_image = utils.load_image(test_input_names[img_ind])
 
     # add the depth as the last channel to input
-    input_depth_image = utils.load_image(test_input_depth_names[ind])
+    input_depth_image = utils.load_image(test_input_depth_names[img_ind])
     if len(input_depth_image.shape) == 3 and input_depth_image.shape[2] == 3:
         input_depth_image = cv2.cvtColor(input_depth_image, cv2.COLOR_BGR2GRAY)
     input_image = np.dstack((input_image, input_depth_image))
@@ -84,7 +96,7 @@ for ind in range(num_test_images):
     input_image = input_image[crp_ys:crp_ye,crp_xs:crp_xe]
     input_image = np.expand_dims(np.float32(input_image),axis=0)/255.0
 
-    gt = utils.load_image(test_output_names[ind])[crp_ys:crp_ye,crp_xs:crp_xe]
+    gt = utils.load_image(test_output_names[img_ind])[crp_ys:crp_ye,crp_xs:crp_xe]
     gt = np.expand_dims(helpers.one_hot_it(gt, label_values, tolerance=50), axis=0)
 
     st = time.time()
@@ -103,10 +115,26 @@ for ind in range(num_test_images):
     gt = helpers.colour_code_segmentation(helpers.reverse_one_hot(gt[0,:,:,:]), label_values)
 
     file_name = os.path.splitext(os.path.basename(test_input_names[ind]))[0]
-    file_dir = "%s/%s_%s"%("Test", args.model, os.path.basename(args.dataset))
     cv2.imwrite("%s/%s_pred.png"%(file_dir, file_name),cv2.cvtColor(np.uint8(out_vis_image), cv2.COLOR_RGB2BGR))
     cv2.imwrite("%s/%s_gt.png"%(file_dir, file_name),cv2.cvtColor(np.uint8(gt), cv2.COLOR_RGB2BGR))
 
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# normalize confusion matrix
+curr_test_miou_op = curr_test_miou_op.astype('float') / curr_test_miou_op.sum(axis=1)[:, np.newaxis]
+
+df = pd.DataFrame(curr_test_miou_op)
+    
+hm = sn.heatmap(df)
+hm.get_figure().savefig(file_dir + "/confusion_matrix.png")
+
+df.to_csv(file_dir+"/confusion_matrix.csv")
+
+with open(file_dir+"/miou.txt", "w") as outf:
+    outf.write("%.3f\n"%curr_test_miou)
+    outf.close()
+
 print("Overall mIOU %.3f"%(curr_test_miou))
-print(curr_test_miou_op)
 print("Average inference time %.3f"%(np.mean(run_times_list)))
